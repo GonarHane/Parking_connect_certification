22f4328e8fdc661681d96be6c405d58483e24f3e
gonarhane <gonar90@gmail.com>
2023-05-04 17:56:36 +0000

    first commit

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,18 @@
+
+import "./App.css";
+
+import Login from "./Components/login/login";
+import Header from "./Components/header/header";
+
+
+
+
+function App() {
+
+  return (
+    <Header />
+    //<Login />//
+  );
+}
+
+export default App;
